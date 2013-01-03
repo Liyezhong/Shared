eff2a7b6fc9244e2e5892a41570b363a5aec4ebc
Nikhil Kamath <Nikhil Kamath@adca880c-22ec-014b-811e-d12c89775d2d>
2013-01-03 11:20:51 +0000

    Implementation for GPIO class
    git-svn-id: https://lmsnus-s-rud1.europe.leicams.com/svn/Colorado_Sepia/trunk/Source/Platform@10718 adca880c-22ec-014b-811e-d12c89775d2d

--- Master/Components/SoftSwitchManager/Include/GPIO.h
+++ Master/Components/SoftSwitchManager/Include/GPIO.h
@@ -7,7 +7,7 @@ namespace SoftSwitchManager {
 class GPIOPin: public QObject {
 private:
     qint32  m_Fd; //!< File descriptor
-    quint32 m_CurrentValue; //!< Current Value of the pin
+    qint32 m_CurrentValue; //!< Current Value of the pin
     quint32 m_PinNumber; //!< GPIO number
     bool    m_Direction; //!< GPIO direction - In/ Out
 
@@ -18,9 +18,9 @@ private:
 public:
     GPIOPin(quint32 PinNumber, quint32 Direction, QObject *p_Parent = NULL);
     ~GPIOPin();
-    void SetEdge(const char *p_Edge);
-    void SetValue(const quint32 Value);
-    quint32 GetValue() const;
+    qint32 SetEdge(const char *p_Edge);
+    qint32 SetValue(const qint32 Value);
+    qint32 GetValue();
 };
 
 } //End of namespace SoftSwitchManager
--- Master/Components/SoftSwitchManager/Source/GPIO.cpp
+++ Master/Components/SoftSwitchManager/Source/GPIO.cpp
@@ -22,7 +22,9 @@ GPIOPin::GPIOPin(quint32 PinNumber, quint32 Direction, QObject *p_Parent)
 
 GPIOPin::~GPIOPin()
 {
-
+    if (0 <= m_Fd) {
+        close(m_Fd);
+    }
 }
 
 void GPIOPin::ExportGPIOPin()
@@ -65,4 +67,87 @@ void GPIOPin::SetDirection(const bool Direction)
     close(Fd);
 }
 
+void GPIOPin::Open()
+{
+    qint32 Length;
+    char Buf[BUF_SIZE];
+    Length = qsnprintf(Buf, sizeof(Buf), SYSFS_GPIO_DIRECTORY"/gpio%d/value", m_PinNumber);
+
+    if ( 0 <= m_Fd ) {
+        close(m_Fd );
+    }
+
+    if( m_Direction )
+        m_Fd = open(Buf, O_RDWR);
+    else
+        m_Fd = open(Buf, O_RDONLY);
+
+    if( 0 > m_Fd ){
+        qDebug()<<" GPIOPin::Open() File Open failed";
+    }
+}
+
+qint32 GPIOPin::GetValue()
+{
+    if( !m_Direction){
+        if ( 0 > m_Fd ) {
+            Open();
+        }
+        if ( 0 > m_Fd ) {
+            return -1;
+        }
+        char Ch;
+
+        read(m_Fd, &Ch, 1);
+
+        if ( Ch != '0' ) {
+            return (m_CurrentValue = 1);
+        } else {
+            return (m_CurrentValue = 0);
+        }
+
+    }
+    return -1;
+}
+
+qint32 GPIOPin::SetEdge(const char *p_Edge)
+{
+    qint32 Length;
+    char Buf[BUF_SIZE];
+    Length = qsnprintf(Buf, sizeof(Buf), SYSFS_GPIO_DIRECTORY"/gpio%d/edge", m_PinNumber);
+
+    m_Fd = open(Buf, O_WRONLY);
+
+    if( m_Fd < 0 ){
+        return -1;
+    }
+
+    write(m_Fd, p_Edge, strlen(p_Edge) + 1);
+
+    close(m_Fd);
+    return 0;
+}
+
+qint32 GPIOPin::SetValue(const qint32 Value)
+{
+    if ( m_Direction && m_CurrentValue != Value ) {
+
+        if ( 0 > m_Fd ) {
+            Open();
+        }
+        if ( 0 > m_Fd ) {
+            return -1;
+        }
+
+        if ( Value ) {
+            write(m_Fd, "1", 2);
+        } else {
+            write(m_Fd, "0", 2);
+        }
+
+        m_CurrentValue = Value;
+    }
+    close(m_Fd);
+    return -1;
+}
 } // End of namespace SoftSwitchManager
