ee91ad99c3799431b26b19569095ffa2e4ec0d01
Bhanu Prasad <Bhanu Prasad@adca880c-22ec-014b-811e-d12c89775d2d>
2013-01-29 05:48:48 +0000

    Removed TestThreads Project from GUI Stub Project Added latest EventStrings.xml file to GUI Stub Project
    git-svn-id: https://lmsnus-s-rud1.europe.leicams.com/svn/Colorado_Sepia/trunk/Source/Platform@11099 adca880c-22ec-014b-811e-d12c89775d2d

--- Master/Components/DataManager/Containers/DeviceConfiguration/Source/DeviceConfiguation.cpp
+++ Master/Components/DataManager/Containers/DeviceConfiguration/Source/DeviceConfiguation.cpp
@@ -166,37 +166,37 @@ bool CDeviceConfiguration::DeserializeContent(QXmlStreamReader& XmlStreamReader,
     SetVersion(XmlStreamReader.attributes().value("Version").toString());
     // Read attribute SLIDEIDCAMERA
     if (!XmlStreamReader.attributes().hasAttribute("SLIDEIDCAMERA")) {
-        qDebug() << "### attribute <Version> is missing => abort reading";
+        qDebug() << "### attribute <SLIDEIDCAMERA> is missing => abort reading";
         Global::EventObject::Instance().RaiseEvent(EVENT_DM_ERROR_XML_ATTRIBUTE_NOT_FOUND, Global::tTranslatableStringList() << "SLIDEIDCAMERA", true);
         return false;
     }
     // Read attribute WORKSTATIONMODE
     if (!XmlStreamReader.attributes().hasAttribute("WORKSTATIONMODE")) {
-        qDebug() << "### attribute <Version> is missing => abort reading";
+        qDebug() << "### attribute <WORKSTATIONMODE> is missing => abort reading";
         Global::EventObject::Instance().RaiseEvent(EVENT_DM_ERROR_XML_ATTRIBUTE_NOT_FOUND, Global::tTranslatableStringList() << "WORKSTATIONMODE", true);
         return false;
     }
     // Read attribute DEVICENAME
     if (!XmlStreamReader.attributes().hasAttribute("DEVICENAME")) {
-        qDebug() << "### attribute <Version> is missing => abort reading";
+        qDebug() << "### attribute <DEVICENAME> is missing => abort reading";
         Global::EventObject::Instance().RaiseEvent(EVENT_DM_ERROR_XML_ATTRIBUTE_NOT_FOUND, Global::tTranslatableStringList() << "DEVICENAME", true);
         return false;
     }
     // Read attribute SERIALNUMBER
     if (!XmlStreamReader.attributes().hasAttribute("SERIALNUMBER")) {
-        qDebug() << "### attribute <Version> is missing => abort reading";
+        qDebug() << "### attribute <SERIALNUMBER> is missing => abort reading";
         Global::EventObject::Instance().RaiseEvent(EVENT_DM_ERROR_XML_ATTRIBUTE_NOT_FOUND, Global::tTranslatableStringList() << "SERIALNUMBER", true);
         return false;
     }
     // Read attribute COVERSLIPPERNAME
     if (!XmlStreamReader.attributes().hasAttribute("COVERSLIPPERNAME")) {
-        qDebug() << "### attribute <Version> is missing => abort reading";
+        qDebug() << "### attribute <COVERSLIPPERNAME> is missing => abort reading";
         Global::EventObject::Instance().RaiseEvent(EVENT_DM_ERROR_XML_ATTRIBUTE_NOT_FOUND, Global::tTranslatableStringList() << "COVERSLIPPERNAME", true);
         return false;
     }
     // Read attribute HEATEDCUEVETTES
     if (!XmlStreamReader.attributes().hasAttribute("HEATEDCUEVETTES")) {
-        qDebug() << "### attribute <Version> is missing => abort reading";
+        qDebug() << "### attribute <HEATEDCUEVETTES> is missing => abort reading";
         Global::EventObject::Instance().RaiseEvent(EVENT_DM_ERROR_XML_ATTRIBUTE_NOT_FOUND, Global::tTranslatableStringList() << "HEATEDCUEVETTES", true);
         return false;
     }
