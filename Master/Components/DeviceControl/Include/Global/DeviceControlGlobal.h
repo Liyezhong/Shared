3a7fb0697be23f1bfb83b3274373a4064e9ef250
Songtao Yu <songtao.yu@biosystems.com>
2014-12-26 18:10:10 +0800

    Enlarge heartbeat time to 2 minutes

--- Master/Components/DeviceControl/Include/Global/DeviceControlGlobal.h
+++ Master/Components/DeviceControl/Include/Global/DeviceControlGlobal.h
@@ -75,7 +75,7 @@ namespace DeviceControl
 #define CAN_NODE_TIMEOUT_INIT                   3000  //!< Timeout initialisation procedure
 #define CAN_NODE_TIMEOUT_CONFIG_RECEIVE         6000  //!< Timeout configuration procedure
 #define CAN_NODE_TIMEOUT_CONFIG_FCT_MODULES     3000  //!< Timeout function module configuration procedure
-#define CAN_NODE_TIMEOUT_HEARTBEAT_FAILURE      5000  //!< Timeout heartbeat failure
+#define CAN_NODE_TIMEOUT_HEARTBEAT_FAILURE      120000  //!< Timeout heartbeat failure
 #define CAN_NODE_TIMEOUT_HEARTBEAT_FAILURE_DBG  60*60*1000  //!< Timeout heartbeat failure (for debug build)
 #define CAN_NODE_TIMEOUT_SETINITOPDATA           500  //!< Timeout setting initial operation data
 #define CAN_NODE_TIMEOUT_DATA_REQ                500  //!< Timeout for several requests with data transfer
